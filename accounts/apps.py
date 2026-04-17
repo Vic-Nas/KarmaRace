84f6dde0e300d1042be7bc08db52381555b5b7a7
Vic-Nas <nasci.victorio@gmail.com>
2026-04-16 20:28:29 -0400

    fix: site_id db ref

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,17 @@
-from django.apps import AppConfig
+# accounts/apps.py
 
+from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib.sites.models import Site
+        from django.conf import settings
+        try:
+            Site.objects.update_or_create(
+                id=settings.SITE_ID,
+                defaults={'domain': settings.DOMAIN, 'name': settings.DOMAIN}
+            )
+        except Exception:
+            pass  # DB not ready yet (e.g. pre-migrate)
